c0c5305ff8db914aa670fdb946ecb70c7c65ab34
Matthias Dittrich <matthi.d@googlemail.com>
2015-03-01 21:57:09 +0100

    Switch to Yaaf.AdvancedBuilding.

--- src/SharedAssemblyInfo.Ninject.cs
+++ src/SharedAssemblyInfo.Ninject.cs
@@ -3,12 +3,12 @@ using System.Reflection;
 
 [assembly: AssemblyCompanyAttribute("Yaaf.DependencyInjection.Ninject")]
 [assembly: AssemblyProductAttribute("Yaaf.DependencyInjection.Ninject")]
-[assembly: AssemblyCopyrightAttribute("Yaaf.DependencyInjection Copyright © Matthias Dittrich 2011-2015")]
-[assembly: AssemblyVersionAttribute("1.0.0.0")]
-[assembly: AssemblyFileVersionAttribute("1.0.0.0")]
+[assembly: AssemblyCopyrightAttribute("Yaaf.DependencyInjection Copyright © Matthias Dittrich 2015")]
+[assembly: AssemblyVersionAttribute("1.0.0")]
+[assembly: AssemblyFileVersionAttribute("1.0.0")]
 [assembly: AssemblyInformationalVersionAttribute("1.0.0")]
 namespace System {
     internal static class AssemblyVersionInformation {
-        internal const string Version = "1.0.0.0";
+        internal const string Version = "1.0.0";
     }
 }
