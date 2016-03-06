88fe403780cdd0bfb386bc3085b6c3efdb13248e
Matthias Dittrich <matthi.d@googlemail.com>
2016-03-06 15:11:19 +0100

    Bump version to 1.1.1 (ninject_ninject_1.1.1, simpleinjector_simpleinjector_1.2.1)

--- src/SharedAssemblyInfo.Ninject.cs
+++ src/SharedAssemblyInfo.Ninject.cs
@@ -4,11 +4,11 @@ using System.Reflection;
 [assembly: AssemblyCompanyAttribute("Yaaf.DependencyInjection.Ninject")]
 [assembly: AssemblyProductAttribute("Yaaf.DependencyInjection.Ninject")]
 [assembly: AssemblyCopyrightAttribute("Yaaf.DependencyInjection Copyright © Matthias Dittrich 2015")]
-[assembly: AssemblyVersionAttribute("1.1.0")]
-[assembly: AssemblyFileVersionAttribute("1.1.0")]
-[assembly: AssemblyInformationalVersionAttribute("1.1.0")]
+[assembly: AssemblyVersionAttribute("1.1.1")]
+[assembly: AssemblyFileVersionAttribute("1.1.1")]
+[assembly: AssemblyInformationalVersionAttribute("1.1.1")]
 namespace System {
     internal static class AssemblyVersionInformation {
-        internal const string Version = "1.1.0";
+        internal const string Version = "1.1.1";
     }
 }
